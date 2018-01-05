46105ef3720e6863ed8e629d604813448c564b38
mark-reardon-momentum <mark.reardon@peoplesmomentum.com>
2018-01-05 14:27:53 +0000

    Added review index page that lists the tracks.

--- review/urls.py
+++ review/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 
-from . import views
+from review import views
 
 app_name = 'review'
 urlpatterns = [
--- review/views.py
+++ review/views.py
@@ -1,4 +1,10 @@
+from django.template import loader
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Track
 
+@login_required
 def index(request):
-    return HttpResponse("Review index.")
+    template = loader.get_template('review/index.html')
+    context = { 'tracks' : Track.objects.all().order_by('display_order') }
+    return HttpResponse(template.render(context, request))
