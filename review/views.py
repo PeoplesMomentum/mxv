e9508cc0f26df7e4aa7d9ce2cca168c4bd843795
mark-reardon-momentum <mark.reardon@peoplesmomentum.com>
2018-01-10 19:29:14 +0000

    Proposals can now be edited and deleted by the member that created them.

--- review/views.py
+++ review/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Track, Theme, Proposal
 from django.db.models import Count
-from review.forms import NewProposalForm
+from review.forms import EditProposalForm, ProposalForm, DeleteProposalForm
 
 @login_required
 def index(request):
@@ -19,23 +19,27 @@ def track(request, pk):
 @login_required
 def theme(request, pk):
     theme = get_object_or_404(Theme, pk = pk)
+    user_proposal = theme.proposals.filter(created_by = request.user).first()
     return render(request, 'review/theme.html', { 
         'theme' : theme, 
-        'proposals': theme.proposals.annotate(nomination_count = Count('nominations')).order_by('-nomination_count', 'created_at') })
+        'proposals': theme.proposals.annotate(nomination_count = Count('nominations')).order_by('-nomination_count', 'created_at'),
+        'user_proposal': user_proposal })
 
 @login_required
 def proposal(request, pk):
     proposal = get_object_or_404(Proposal, pk = pk)
     proposal.views += 1
     proposal.save()
+    form = ProposalForm(instance = proposal)
     return render(request, 'review/proposal.html', { 
-        'proposal' : proposal })
+        'proposal' : proposal,
+        'form': form })
 
 @login_required
 def new_proposal(request, pk):
     theme = get_object_or_404(Theme, pk = pk)
     if request.method == "POST":
-        form = NewProposalForm(request.POST)
+        form = EditProposalForm(request.POST)
         if form.is_valid():
             proposal = form.save(commit = False)
             proposal.theme = theme
@@ -43,7 +47,35 @@ def new_proposal(request, pk):
             proposal.save()
             return redirect('review:proposal', pk = proposal.pk)
     else:
-        form = NewProposalForm()
+        form = EditProposalForm()
     return render(request, 'review/new_proposal.html', { 
         'theme' : theme,
+        'form' : form })
+
+@login_required
+def edit_proposal(request, pk):
+    proposal = get_object_or_404(Proposal, pk = pk)
+    if request.method == "POST":
+        form = EditProposalForm(request.POST, instance = proposal)
+        if form.is_valid():
+            proposal = form.save()
+            return redirect('review:proposal', pk = proposal.pk)
+    else:
+        form = EditProposalForm(instance = proposal)
+    return render(request, 'review/edit_proposal.html', { 
+        'proposal' : proposal,
+        'form' : form })
+
+@login_required
+def delete_proposal(request, pk):
+    proposal = get_object_or_404(Proposal, pk = pk)
+    if proposal and proposal.created_by == request.user and request.method == "POST":
+        form = DeleteProposalForm(request.POST, instance = proposal)
+        if form.is_valid():
+            proposal.delete()
+            return redirect('review:theme', pk = proposal.theme.pk)
+    else:
+        form = DeleteProposalForm(instance = proposal)
+    return render(request, 'review/delete_proposal.html', { 
+        'proposal' : proposal,
         'form' : form })
