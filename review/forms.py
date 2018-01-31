3db62763f938256d3e266d97e194d0381712e0b4
mark-reardon-momentum <mark.reardon@peoplesmomentum.com>
2018-01-31 15:36:20 +0000

    Increased text areas to 8 rows and improved proposal layout.

--- review/forms.py
+++ review/forms.py
@@ -1,13 +1,15 @@
 from django import forms
 from .models import Proposal, Amendment, Comment, ModerationRequest, name_length, text_length, summary_length
 
+TEXT_FIELD_ROWS = 8
+
 class ProposalForm(forms.ModelForm):
     name = forms.CharField(
         widget = forms.TextInput(attrs = { 'readonly': True }))
     summary = forms.CharField(
-        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': 5 }))
+        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': TEXT_FIELD_ROWS }))
     text = forms.CharField(
-        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': 5 }))
+        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': TEXT_FIELD_ROWS }))
 
     class Meta:
         model = Proposal
@@ -19,11 +21,11 @@ class EditProposalForm(forms.ModelForm):
         max_length = name_length,
         help_text = "The maximum length of the name is %d characters" % name_length)
     summary = forms.CharField(
-        widget = forms.Textarea(attrs = { 'rows': 5, 'placeholder': "Please summarise your proposal" }), 
+        widget = forms.Textarea(attrs = { 'rows': TEXT_FIELD_ROWS, 'placeholder': "Please summarise your proposal" }), 
         max_length = summary_length, 
         help_text = "The maximum length of the text is %d characters" % summary_length)
     text = forms.CharField(
-        widget = forms.Textarea(attrs = { 'rows': 5, 'placeholder': "What is the full text of your proposal?" }), 
+        widget = forms.Textarea(attrs = { 'rows': TEXT_FIELD_ROWS, 'placeholder': "What is the full text of your proposal?" }), 
         max_length = text_length, 
         help_text = "The maximum length of the text is %d characters" % text_length)
 
@@ -35,9 +37,9 @@ class DeleteProposalForm(forms.ModelForm):
     name = forms.CharField(
         widget = forms.TextInput(attrs = { 'readonly': True }))
     summary = forms.CharField(
-        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': 5 }))
+        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': TEXT_FIELD_ROWS }))
     text = forms.CharField(
-        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': 5 }))
+        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': TEXT_FIELD_ROWS }))
      
     class Meta:
         model = Proposal
@@ -47,7 +49,7 @@ class AmendmentForm(forms.ModelForm):
     name = forms.CharField(
         widget = forms.TextInput(attrs = { 'readonly': True }))
     text = forms.CharField(
-        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': 5 }))
+        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': TEXT_FIELD_ROWS }))
 
     class Meta:
         model = Amendment
@@ -59,7 +61,7 @@ class EditAmendmentForm(forms.ModelForm):
         max_length = name_length,
         help_text = "The maximum length of the name is %d characters" % name_length)
     text = forms.CharField(
-        widget = forms.Textarea(attrs = { 'rows': 5, 'placeholder': "What is your amendment?" }), 
+        widget = forms.Textarea(attrs = { 'rows': TEXT_FIELD_ROWS, 'placeholder': "What is your amendment?" }), 
         max_length = text_length, 
         help_text = "The maximum length of the text is %d characters" % text_length)
 
@@ -71,7 +73,7 @@ class DeleteAmendmentForm(forms.ModelForm):
     name = forms.CharField(
         widget = forms.TextInput(attrs = { 'readonly': True }))
     text = forms.CharField(
-        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': 5 }))
+        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': TEXT_FIELD_ROWS }))
      
     class Meta:
         model = Amendment
@@ -79,7 +81,7 @@ class DeleteAmendmentForm(forms.ModelForm):
 
 class EditCommentForm(forms.ModelForm):
     text = forms.CharField(
-        widget = forms.Textarea(attrs = { 'rows': 5, 'placeholder': "What is your comment?" }), 
+        widget = forms.Textarea(attrs = { 'rows': TEXT_FIELD_ROWS, 'placeholder': "What is your comment?" }), 
         max_length = text_length, 
         help_text = "The maximum length of the text is %d characters" % text_length)
 
@@ -91,7 +93,7 @@ class ModerationRequestForm(forms.ModelForm):
     def __init__(self, entity, *args,**kwargs):
         super(ModerationRequestForm, self).__init__(*args, **kwargs)
         self.fields['reason'] = forms.CharField(
-            widget = forms.Textarea(attrs = { 'rows': 5, 'placeholder': "Why does this %s require moderation?" % entity }), 
+            widget = forms.Textarea(attrs = { 'rows': TEXT_FIELD_ROWS, 'placeholder': "Why does this %s require moderation?" % entity }), 
             max_length = text_length, 
             help_text = "The maximum length of the reason is %d characters" % text_length)
 
@@ -101,7 +103,7 @@ class ModerationRequestForm(forms.ModelForm):
 
 class CommentForm(forms.ModelForm):
     text = forms.CharField(
-        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': 5 }))
+        widget = forms.Textarea(attrs = { 'readonly': True, 'rows': TEXT_FIELD_ROWS }))
 
     class Meta:
         model = Comment
