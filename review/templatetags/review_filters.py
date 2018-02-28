bfeafb905b9da3e2f43e3529815227a30ee59756
mark-reardon-momentum <mark.reardon@peoplesmomentum.com>
2018-02-28 12:58:03 +0000

    Completed voting template and improved vote results presentation.

--- review/templatetags/review_filters.py
+++ review/templatetags/review_filters.py
@@ -12,7 +12,10 @@ def question_and_choices(vote, question_id):
     question_count = vote.track_voting.questions.count()
     question = vote.track_voting.questions.filter(pk=question_id).first()
     question_html = 'Question %d of %d - %s' % (question.number, question_count, question.text)
-        
+
+    # answer        
+    current_answer = vote.answers.filter(question = question).first()
+
     # choices
     choices = question.choices.all()
     today = date.today()
@@ -24,15 +27,21 @@ def question_and_choices(vote, question_id):
         
     # voting in progress
     elif vote.track_voting.voting_in_range():
-        current_answer = vote.answers.filter(question = question).first()
         for choice in choices:
             choice_html += '<label class="radio-inline mt-4 mr-4"><input type="radio" name="answer_%d" value="%d_%d" %s>%s</label>' % (question.id, question.id, choice.id, 'checked="checked"' if current_answer and current_answer.choice == choice else '', choice.text)
     
     # voting complete
     else:
+        # vote counts
         for choice in choices:
             choice_count = choice.answers.filter(question__pk = question_id).count()
             choice_html += '<label class="mt-4 mr-4">%s - %d vote%s</label>' % (choice.text, choice_count, '' if choice_count == 1 else 's')
+        
+        # member's choice
+        if current_answer:
+            choice_html += '<label class="mt-4 mr-4">(You voted %s)</label>' % current_answer.choice.text
+        else:
+            choice_html += '<label class="mt-4 mr-4">(You didn''t vote on this question)</label>'
     
     # vote
     vote_html = '<button type="submit" name="vote" class="btn btn-primary">Update votes</button>' if vote.track_voting.voting_in_range() else ''
