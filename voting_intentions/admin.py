54d8292e9eae236f9a4ae9b9bb16795aa25b111d
mark-reardon-momentum <mark.reardon@peoplesmomentum.com>
2018-07-12 09:09:42 +0100

    Added CSV export of intentions to vote admin.

--- voting_intentions/admin.py
+++ voting_intentions/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from voting_intentions.models import Vote
 from django.utils.safestring import mark_safe
 from django import forms
+from django.http.response import HttpResponse
+import csv
 
 class VoteAdminForm(forms.ModelForm):
     class Meta:
@@ -31,7 +33,7 @@ class VoteAdmin(admin.ModelAdmin):
         total = vote.intentions.all().count()
         for choice in vote.choices.all():
             count = vote.intentions.filter(choice = choice).count()
-            row = '<tr><td>%s</td><td>%d</td><td>%.2f</td></tr>' % (choice.text, count, count / total * 100 if total > 0 else 0)
+            row = '<tr><td>%d - %s</td><td>%d</td><td>%.2f</td></tr>' % (choice.id, choice.text, count, count / total * 100 if total > 0 else 0)
             rows.append(row)
         
         # table
@@ -42,5 +44,28 @@ class VoteAdmin(admin.ModelAdmin):
         
         return mark_safe(table)
     results_table.short_description = 'results'
+    
+    # returns the intentions as a CSV response
+    def response_change(self, request, obj):
+        if 'export_intentions_as_csv' in request.POST:
+            vote = obj
+
+            # create the response and CSV
+            response = HttpResponse(content_type = 'text/csv')
+            response['Content-Disposition'] = 'attachment; filename=%s.csv' % vote.name
+            writer = csv.writer(response, quoting = csv.QUOTE_ALL)
+            
+            # header
+            writer.writerow(['email', 'nation_builder_id', 'time', 'vote', 'choice' ])
+            
+            # intentions
+            for intention in vote.intentions.all():
+                writer.writerow([intention.email, intention.nation_builder_id, intention.recorded_at, intention.vote_id, intention.choice_id])
+                
+            return response
+            
+        # call the inherited
+        return admin.ModelAdmin.response_change(self, request, obj)
+
 
 admin.site.register(Vote, VoteAdmin)
