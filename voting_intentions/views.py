96e67462a0ed95638cd580283b87a4d35c013980
mark-reardon-momentum <mark.reardon@peoplesmomentum.com>
2018-07-20 12:29:31 +0100

    Changed default redirect page to Momentum home page.

--- voting_intentions/views.py
+++ voting_intentions/views.py
@@ -1,55 +1,58 @@
 from django.shortcuts import redirect
 from voting_intentions.models import Vote, Choice
 from mxv.nation_builder import NationBuilder
-from mxv.settings import DEFAULT_DONATION_PAGE_URL
+from mxv.settings import DEFAULT_REDIRECT_PAGE_URL
 
 # records a voting intention from the URL parameters and redirects
 def index(request):
     
-    # if the email, vote and intention are in the URL parameters...
-    vote_id = request.GET.get('vote', None)
-    choice_id = request.GET.get('choice', None)
-    vote = Vote.objects.filter(id = vote_id).first()
-    choice = Choice.objects.filter(id = choice_id).first()
-    email = request.GET.get('email', None)
-    if vote and choice and email:
-        
-        # get the NB id for the email    
-        nb = NationBuilder()
-        nb_id = nb.IdFromEmail(email)
-
-        # record a voting intention
-        vote.intentions.create(vote = vote, choice = choice, email = email, nation_builder_id = nb_id)
-        
-        # set the vote and choice tags
-        if nb_id:        
-            # add tags
-            add_tags = vote.vote_tags.filter(add=True).union(choice.choice_tags.filter(add=True))
-            nb.SetPersonTags(nb_id, [tag.text for tag in add_tags])
+    try:
+        # if the email, vote and intention are in the URL parameters...
+        vote_id = request.GET.get('vote', None)
+        choice_id = request.GET.get('choice', None)
+        vote = Vote.objects.filter(id = vote_id).first()
+        choice = Choice.objects.filter(id = choice_id).first()
+        email = request.GET.get('email', None)
+        if vote and choice and email:
             
-            # remove tags
-            remove_tags = vote.vote_tags.filter(add=False).union(choice.choice_tags.filter(add=False))
-            nb.ClearPersonTags(nb_id, [tag.text for tag in remove_tags])
-        
-        # for each URL parameter that is present in the request...
-        url_parameters_present = []
-        for url_parameter in vote.url_parameters.all():
-            if url_parameter.name in request.GET:
-                
-                # change its name if necessary
-                name = url_parameter.name if url_parameter.pass_on_name == '' else url_parameter.pass_on_name
-                value = request.GET[url_parameter.name]
-                url_parameters_present.append((name, value))
-                
-        # pass the parameters on
-        url_parameter_string = '&'.join('='.join(present) for present in url_parameters_present)
-
-        # redirect
-        return redirect('?'.join([vote.redirect_url, url_parameter_string]))
+            # get the NB id for the email    
+            nb = NationBuilder()
+            nb_id = nb.IdFromEmail(email)
     
-    else:
-        # malformed request so just redirect to thanks/donation page
-        if vote:
-            return redirect(vote.redirect_url)
+            # record a voting intention
+            vote.intentions.create(vote = vote, choice = choice, email = email, nation_builder_id = nb_id)
+            
+            # set the vote and choice tags
+            if nb_id:        
+                # add tags
+                add_tags = vote.vote_tags.filter(add=True).union(choice.choice_tags.filter(add=True))
+                nb.SetPersonTags(nb_id, [tag.text for tag in add_tags])
+                
+                # remove tags
+                remove_tags = vote.vote_tags.filter(add=False).union(choice.choice_tags.filter(add=False))
+                nb.ClearPersonTags(nb_id, [tag.text for tag in remove_tags])
+            
+            # for each URL parameter that is present in the request...
+            url_parameters_present = []
+            for url_parameter in vote.url_parameters.all():
+                if url_parameter.name in request.GET:
+                    
+                    # change its name if necessary
+                    name = url_parameter.name if url_parameter.pass_on_name == '' else url_parameter.pass_on_name
+                    value = request.GET[url_parameter.name]
+                    url_parameters_present.append((name, value))
+                    
+            # pass the parameters on
+            url_parameter_string = '&'.join('='.join(present) for present in url_parameters_present)
+    
+            # redirect
+            return redirect('?'.join([vote.redirect_url, url_parameter_string]))
+        
         else:
-            return redirect(DEFAULT_DONATION_PAGE_URL)
+            # malformed request so just redirect to thanks/donation page
+            if vote:
+                return redirect(vote.redirect_url)
+            else:
+                return redirect(DEFAULT_REDIRECT_PAGE_URL)
+    except:
+        return redirect(DEFAULT_REDIRECT_PAGE_URL)
